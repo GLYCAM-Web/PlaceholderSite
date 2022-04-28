1ce9baa4de5446702e5662502bde81bc121cc100
Dan Wentworth <danwentworthart@gmail.com>
2022-04-28 16:45:21 -0400

    Adds a head and some meta tags in attempt to prevent caching.

--- make_html.bash
+++ make_html.bash
@@ -5,9 +5,20 @@
 ##
 ## The variable "DOMAIN" should be defined in the calling script (start.bash)
 ##
+## Warning, copy pasta:
+## Attempting to prevent page caching. This is done by adding the head and meta
+##		tags, as demonstrated on this site. Hard to test until deployed. 
+##		https://cristian.sulea.net/blog/disable-browser-caching-with-meta-html-tags/
+
 #
 # For develpment sites:
 htmlToDeploy="""
+<head>
+<meta http-equiv='Cache-Control' content='no-cache, no-store, must-revalidate' />
+<meta http-equiv='Pragma' content='no-cache' />
+<meta http-equiv='Expires' content='0' />
+</head>
+<body>
 <h1>Hello from ${DOMAIN}!</h1>
 The development site that normally lives at ${DOMAIN} is on bardo right now.
 It should be reincarnated soon. </p>
@@ -27,6 +38,7 @@ The site that normally lives at ${DOMAIN} is down for maintenance.
 The development team has been informed.  </p>
 ${TimeFixedText} </p>
 We apologize for any inconvenience. </p>
+</body>
 """
 fi
 
